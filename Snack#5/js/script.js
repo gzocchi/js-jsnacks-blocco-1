b3359e56930a06acd4f2283df465eb475ea64223
gzocchi <z.giorgio.z@gmail.com>
2021-05-04 14:39:57 +0200

    aggiunto script Snack #5

--- Snack#5/js/script.js
+++ Snack#5/js/script.js
@@ -3,3 +3,18 @@
 // Chiedi per 6 volte all’utente di inserire un numero,
 // se è dispari (verifichiamolo con una funzione) inseriscilo nell’array.
 // Stampa l'array nella console.
+var numberArray = [];
+
+for (var i = 0; i < 6; i++) {
+    var question = parseInt(prompt("Dimmi un numero"));
+    if (isOdd(question)) {
+        numberArray.push(question);
+    };
+};
+
+console.log("I numeri dispari tra quelli indicati sono:", numberArray);
+
+// FUNCTION
+function isOdd (num) {
+    return num % 2 != 0;
+}
